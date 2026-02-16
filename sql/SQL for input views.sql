e17d590b98f5db06c78d42fef0713485005132a8
ErikaDAnderson <Erika.Anderson@dfo-mpo.gc.ca>
2026-02-16 12:25:26 -0800

    update schemas for views

--- sql/SQL for input views.sql
+++ sql/SQL for input views.sql
@@ -1,10 +1,10 @@
 --sql to generate input files for scripts
---Run on NuSEDS PROD V2
 --export three excel files and save into date folder
 --Erika Anderson 2026-02-05
 
 
 --Conserv Unit System Sites MV
+--run on rpt schema
 --save as conserv_unit_system_sites_mv.xlsx
 
 SELECT *
@@ -12,12 +12,14 @@ SELECT *
 
 
 --Cu Profile View
+--Run on NuSEDS PROD V2
 --save as cu_profile_vw.xlsx
 
 SELECT *
   FROM CU_PROFILE_VW;
 
 --Geofeatures
+--Run on NuSEDS PROD V2
 --save as geo_features.xlsx
 
 SELECT *
